46632024ae3a6074d9928f9c862189b22f0846f9
Sindy0711 <anhhuy071103@gmail.com>
2026-01-20 00:59:26 +0700

    feat: add layout components and global styles for improved UI structure

---
+++ frontend/src/components/channel/ChannelSidebar.tsx
@@ -0,0 +1,7 @@
+
+
+export default function ChannelSidebar() {
+  return (
+    <div>ChannelSidebar</div>
+  )
+}
--- frontend/src/layout/AppLayout.tsx
+++ frontend/src/layout/AppLayout.tsx
@@ -1,18 +1,13 @@
-import SideBar from "../components/server/ServerSidebar";
-import Home from "../pages/Home";
+import ServerSidebar from "../components/server/ServerSidebar";
+import ChannelSidebar from "../components/channel/ChannelSidebar";
+import MainContent from "../components/main/MainContent";
 
 export default function AppLayout() {
   return (
-    <div className="d-flex vh-100">
-      {/* <ServerSidebar /> */}
-      <div style={{ width: "250px" }}>
-        <SideBar />
-      </div>
-
-      {/* MainContent */}
-      <main className="flex-grow-1 p-3">
-        <Home />
-      </main>
+    <div className="app-root">
+      <ServerSidebar />
+      <ChannelSidebar />
+      <MainContent />
     </div>
   );
 }
